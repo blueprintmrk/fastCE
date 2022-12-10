402095bff7004a3966f7cee1e7926608bc59bd7f
Blueprint Marketing <blueprintmrk@users.noreply.github.com>
2022-12-09 21:14:10 -0500

    Add source code from Fiddle

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,86 +1,24 @@
+import { Response, Request, Fastly } from "@fastly/as-compute";
+import { JSON } from "assemblyscript-json"; 
 
-//! Default Compute@Edge template program.
-import { Request, Response, Headers, URL, Fastly } from "@fastly/as-compute";
+const BACKEND_NAME = "origin_0";
 
-// The entry point for your application.
-//
-// Use this function to define your main request handling logic. It could be
-// used to route based on the request properties (such as method or path), send
-// the request to a backend, make completely new requests, and/or generate
-// synthetic responses.
+// Make a request for some JSON
+let req = new Request("https://httpbin.org/anything", {
+  method: "GET",
+  headers: null,
+  body: null,
+});
+let jsonResp = Fastly.fetch(req, {
+  backend: BACKEND_NAME
+}).wait();
 
-function main(req: Request): Response {
-    // Filter requests that have unexpected methods.
-    if (!["HEAD", "GET"].includes(req.method)) {
-        return new Response(String.UTF8.encode("This method is not allowed"), {
-            status: 405,
-            headers: null,
-            url: null
-        });
-    }
+// Parse the JSON from the request
+let jsonObj: JSON.Obj = changetype<JSON.Obj>(JSON.parse(jsonResp.text()));
+jsonObj.set("new_field", JSON.from("content"));
 
-    let url = new URL(req.url);
-
-    // If request is to the `/` path...
-    if (url.pathname == "/") {
-        // Below are some common patterns for Compute@Edge services using AssemblyScript.
-        // Head to https://developer.fastly.com/learning/compute/assemblyscript/ to discover more.
-
-        // Create a new request.
-        // let bereq = new Request("http://example.com", {
-        //     method: "GET",
-        //     headers: null,
-        //     body: null
-        // });    
-
-        // Add request headers.
-        // req.headers.set("X-Custom-Header", "Welcome to Compute@Edge!");
-        // req.headers.set(
-        //   "X-Another-Custom-Header",
-        //   "Recommended reading: https://developer.fastly.com/learning/compute"
-        // );
-
-        // Create a cache override.
-        // let cacheOverride = new Fastly.CacheOverride();
-        // cacheOverride.setTTL(60);
-
-        // Forward the request to a backend.
-        // let beresp = Fastly.fetch(req, {
-        //     backend: "backend_name",
-        //     cacheOverride,
-        // }).wait();
-
-        // Remove response headers.
-        // beresp.headers.delete("X-Another-Custom-Header");
-
-        // Log to a Fastly endpoint.
-        // const logger = Fastly.getLogEndpoint("my_endpoint");
-        // logger.log("Hello from the edge!");
-
-        // Send a default synthetic response.
-        let headers = new Headers();
-        headers.set('Content-Type', 'text/html; charset=utf-8');
-
-        return new Response(String.UTF8.encode("<iframe src='https://developer.fastly.com/compute-welcome' style='border:0; position: absolute; top: 0; left: 0; width: 100%; height: 100%'></iframe>\n"), {
-            status: 200,
-            headers,
-            url: null
-        });
-    }
-
-    // Catch all other requests and return a 404.
-    return new Response(String.UTF8.encode("The page you requested could not be found"), {
-        status: 404,
-        headers: null,
-        url: null
-    });
-}
-
-// Get the request from the client.
-let req = Fastly.getClientRequest();
-
-// Pass the request to the main request handler function.
-let resp = main(req);
-
-// Send the response back to the client.
+let resp = new Response(String.UTF8.encode(jsonObj.toString()), {
+  status: 200,
+  headers: null,
+});
 Fastly.respondWith(resp);
